23d1c7f40b8ad19846ed058beb3f5b31e4ec7201
Michael Oliver <majinichi@gmail.com>
2017-04-06 10:05:26 -0700

    Update the plugin to try to use the dropdown label from the DOM on-init
    Added the ability to retrieve the HTML used for the dropdown label on initialization and use that whenever an item in the dropdown menu is not active instead of "Dropdown".  This includes the HTML used for the caret.
    
    Also fixing a typo in the readme.

--- jquery.droptabs.js
+++ jquery.droptabs.js
@@ -19,33 +19,35 @@
         }, o);
 
         return this.each( function(  ) {
-		
+
 			var $container = $(this);
 			var dropdown = $(s.dropdownSelector, this);
 			var dropdownMenu = $(s.dropdownMenuSelector, dropdown);
-			
+			// Get the initial dropdown label (caret and all).
+			var dropdownLabel = $('>a', dropdown).html();
+
 			var $dropdownTabs = function () {
-				return $(s.dropdownTabsSelector, dropdownMenu);			
+				return $(s.dropdownTabsSelector, dropdownMenu);
 			}
-			
+
 			var $visibleTabs = function () {
 				return $(s.visibleTabsSelector, $container);
 			}
-			
-			function getFirstHiddenElementWidth() {			
+
+			function getFirstHiddenElementWidth() {
 				var tempElem=$dropdownTabs().first().clone().appendTo($container).css("position","fixed");
 				var hiddenElementWidth = $(tempElem).outerWidth();
 				$(tempElem).remove();
 				return hiddenElementWidth;
 			}
-			
+
 			function getHiddenElementWidth(elem) {
 				var tempElem=$(elem).clone().appendTo($container).css("position","fixed");
 				var hiddenElementWidth = $(tempElem).outerWidth();
 				$(tempElem).remove();
 				return hiddenElementWidth;
 			}
-			
+
 			function manageActive(elem) {
 				//fixes a bug where Bootstrap can't remove the 'active' class on elements after they've been hidden inside the dropdown
 				$('a', $(elem)).on('show.bs.tab', function (e) {
@@ -54,20 +56,23 @@
 				$('a', $(elem)).on('shown.bs.tab', function (e) {
 					if ($(dropdown).hasClass('active')) {
 						$('>a', dropdown).html(($('>li.active>a', dropdownMenu).html()).substring(0,10) + '... <b class="caret"></b>');
+					} else if (dropdownLabel) {
+						// Try to use the dropdown label used on-create
+						$('>a', dropdown).html(dropdownLabel);
 					} else {
-						$('>a', dropdown).html('Dropdown <b class="caret"></b>'); 
+						$('>a', dropdown).html('Dropdown <b class="caret"></b>');
 					}
 				})
-				
+
 			}
-			
+
 			//Start Development info
-			if ( s.development ) {		
+			if ( s.development ) {
 				$('body').append('<div class="alert alert-success" id="'+ s.developmentId +'"></div>');
 				var $developmentDiv = $('#' + s.developmentId);
-				$($developmentDiv).css('position','fixed').css('right','20px').css('bottom','20px');				
+				$($developmentDiv).css('position','fixed').css('right','20px').css('bottom','20px');
 				function devPrint(label, elem) {
-				var labelId = label.replace(/\s+/g, '-').toLowerCase();				
+				var labelId = label.replace(/\s+/g, '-').toLowerCase();
 					if ($('#'+labelId).length > 0) {
 						$('#'+labelId).text(label + ': ' + elem);
 					} else {
@@ -75,9 +80,9 @@
 					}
 					return true;
 				}
-			}		
+			}
 			//End Development info
-			
+
 			var visibleTabsWidth = function () {
 				var visibleTabsWidth = 0;
 				$($visibleTabs()).each(function( index ) {
@@ -86,47 +91,47 @@
 				visibleTabsWidth = visibleTabsWidth + parseInt($(dropdown).outerWidth(), 10);
 			return visibleTabsWidth;
 			}
-			
+
 			var availableSpace = function () {
 				return $container.outerWidth()-visibleTabsWidth();
 			}
-			
+
 			var arrangeTabs = function () {
 				//Start Development info
-				if ( s.development ) {		
+				if ( s.development ) {
 					devPrint("Container width", $container.outerWidth());
 					devPrint("Visible tabs width", visibleTabsWidth());
 					devPrint("Available space", availableSpace());
 					devPrint("First hidden", getFirstHiddenElementWidth());
 				}
-				//End Development info	
-				
+				//End Development info
+
 				if (availableSpace()<0) {//we will hide tabs here
 					var x = availableSpace();
 					$($visibleTabs().get().reverse()).each(function( index ){
-						if (!($(this).hasClass('always-visible'))){	
+						if (!($(this).hasClass('always-visible'))){
 								$(this).prependTo(dropdownMenu);
 								x=x+$(this).outerWidth();
-						}  
+						}
 						if (x>=0) {return false;}
 					});
 				}
-				
+
 				if (availableSpace()>getFirstHiddenElementWidth()) { //and here we bring the tabs out
 					var x = availableSpace();
 					$($dropdownTabs()).each(function( index ){
-						if (getHiddenElementWidth(this) < x && !($(this).hasClass('always-dropdown'))){	
-							$(this).appendTo($container);	
+						if (getHiddenElementWidth(this) < x && !($(this).hasClass('always-dropdown'))){
+							$(this).appendTo($container);
 							x = x-$(this).outerWidth();
 						} else {return false;}
 					 });
 				}
-				
+
 				if ($dropdownTabs().length <= 0) {dropdown.hide();} else {dropdown.show();}
 			}
-			
+
 			//init
-			
+
 			if (s.autoArrangeTabs) {
 				var tempTabs = [];
 				$($visibleTabs().get().reverse()).each(function( index ){
@@ -139,22 +144,22 @@
 					$container.prepend(tempTabs[i]);
 				}
 			}
-			
+
 			$(document).ready(function(){
 				arrangeTabs();
 				$dropdownTabs().each( function() {
 					manageActive($(this));
 				});
-				
+
 				$visibleTabs().each( function() {
 					manageActive($(this));
 				});
 			});
-			
+
 			$( window ).resize(function() {
 				arrangeTabs();
 			});
-			return this;		
+			return this;
         });
     }
 }(jQuery));
